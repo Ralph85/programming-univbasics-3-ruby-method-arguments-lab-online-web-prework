494599f4910b05a4c0b9bc89232e67eb31c55b02
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-07-22 14:19:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,6 +10,6 @@ def introduction_with_language_optional(name)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional(language="default", name)
+def introduction_with_language_optional(language="Ruby", name)
   puts "Hi, my name is #{"Edwin"} and I am learning to program in #{"Python"}."
 end
